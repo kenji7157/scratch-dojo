5beba861a0fa69a0917e687161c231dfb44aacad
kenji7157 <kawake.kk@gmail.com>
2022-12-21 17:33:05 +0900

    画像ファイル名をわかりやすくなるよう修正

--- jest.storyshotsImage.ts
+++ jest.storyshotsImage.ts
@@ -1,6 +1,19 @@
 import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+import path from 'path';
 
 initStoryshots({
-  test: imageSnapshot(),
+  test: imageSnapshot({
+    getMatchOptions: (options) => {
+      const { kind, story } = options.context;
+
+      const dir = path.resolve(__dirname, '__image_snapshots__', kind);
+      const name = story.replaceAll(/\s/g, '');
+
+      return {
+        customSnapshotsDir: path.resolve(dir),
+        customSnapshotIdentifier: name,
+      };
+    },
+  }),
 });
